0d8f031df2c9cb7a142861ee047f9c5dd875a73b
Paul Easter <paul.john.easter@gmail.com>
2023-04-05 13:11:07 +1000

    Added in Rating field to working database

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -12,7 +12,8 @@ public class Movie
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
-    
+
     [Column(TypeName = "decimal(18, 2)")]
-    public decimal Price { get; set; }   
+    public decimal Price { get; set; }
+    public string Rating { get; set; } = string.Empty;
 }
